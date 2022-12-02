9d28b597e04786d2a8ed4749bc29a8c89f8eb0a6
Mr-Raja12 <rk9576899082@gmail.com>
2022-12-03 03:56:59 +0530

    Added: As a Gambler make  bet so either win or loose .

--- src/com/gamblinsimulation/GamblingSimulationProblem.java
+++ src/com/gamblinsimulation/GamblingSimulationProblem.java
@@ -1,4 +1,5 @@
 package com.gamblinsimulation;
+import java.util.Random;
 /**
  * @author Raja 
  * This program is to display the Welcome message to Gambling Simulation Program.
@@ -8,25 +9,58 @@ public class GamblingSimulationProblem {
     /**
      * Default construction
      */
+
+    //Default Constructor
     public GamblingSimulationProblem() {
         System.out.println("Welcome to Gambling Simulation Program");
         System.out.println();
         System.out.println("Gambler starts the game...");
     }
-    //Declaration constants
+
+    /**
+     * Declaring constants
+     */
     final int betMoney = 1;
     final int stakePerDay = 100;
+
     public static void main(String[] args) {
 
-        //Initialize the object.
+        /**
+         * Initializing the object.
+         */
         GamblingSimulationProblem gambling = new GamblingSimulationProblem();
         System.out.println();
-
-        //Declaration variables
-        int stake = 0;
-        stake = stake + gambling.stakePerDay;
         System.out.println("Initial stake of Gambler is : "+gambling.stakePerDay);
         System.out.println("Initial betting money in gambling is : "+gambling.betMoney);
+        startsGambling(gambling);
+    }
+     /**
+     * Method for starting the game.
+     * @param gambling
+     */
+    public static void startsGambling(GamblingSimulationProblem gambling) {
+        //Declaring variables
+        Random random = new Random();
+        int stake = 0;
+        stake = stake + gambling.stakePerDay;
+        int bet = random.nextInt(2);
+
+        /**
+         * Checking randomly if win or loose.
+         */
+        switch (bet)
+        {
+            case 1 :
+                System.out.println("Gambler won the bet.");
+                stake = stake + gambling.betMoney;
+                break;
+            default :
+                System.out.println("Gambler lost the bet.");
+                stake = stake - gambling.betMoney;
+                break;
+        }
+
+        System.out.println("Amount remaining at Gambler is : "+stake);
 
     }
 }
